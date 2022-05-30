f30302893362b12af90cb16602aa8c57d95cb903
Gabrielondono <84252501+Gabrielondono@users.noreply.github.com>
2022-05-29 21:07:11 -0500

    img commit
    imagenes y funcionalidad

--- sketch.js
+++ sketch.js
@@ -1,25 +1,45 @@
 let exe = true;
 let i = 0;
 let a = 1;
+let n = 3;
+let c = 256;
+ex = true;
+imagenes = [];
+
+function preload() {
+	fondo = loadImage("assets/2.jpg")
+   for( let g = 0; g <= 40; g++){
+	eval('img'+g+' = loadImage("assets/'+(10+g)+'.jpg")')
+	eval('append(imagenes, img'+g+')')
+   }
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-//tamaño circulo del nodo inicial
-  c_nodo0 = new CNodo( windowWidth/15, windowHeight/2);
+  createCanvas(windowWidth*1.5, windowHeight);
+  c_nodo0 = new CNodo( windowWidth/15, windowHeight/2, n , 0 , c , ex , picture());
   nodo0 = new Node(0);
-  execute();
+  execute(n);
 }
 
 function draw() {
-  background(69);
-  c_nodo0.dr();
+  background(fondo);
+  strokeWeight(6);
   fig(i) ;
+  c_nodo0.dr();
+  if(i==7){
+    exe = false
+  }
 }
 
 function fig(i){
   for (let p = 0; p < i+1; p++){
     for (let j = 1; j < 6; j++){
-      eval('c_nodo'+(j+(5*p))+'.dr()');
+      eval('c_nodo'+(j+(5*p))+'.ln()');
+    }
+  }
+  for (let p = 0; p < i+1; p++){
+    for (let j = 1; j < 6; j++){
+	eval('c_nodo'+(j+(5*p))+'.dr()');
     }
   }
 }
@@ -28,76 +48,121 @@ class Node{
   constructor(val) {
     this.val = val;
   }
-	multi(i){
-		this.n1 = new Node(1+(5*i));
-		this.n2 = new Node(2+(5*i));
-		this.n3 = new Node(3+(5*i));
-		this.n4 = new Node(4+(5*i));
-		this.n5 = new Node(5+(5*i));
+   multi(i){
+   	this.n1 = new Node(1+(5*i));
+   	this.n2 = new Node(2+(5*i));
+   	this.n3 = new Node(3+(5*i));
+   	this.n4 = new Node(4+(5*i));
+   	this.n5 = new Node(5+(5*i));
   }
 }
 
 class CNodo{
-  constructor(posx, posy) {
+  constructor(posx, posy, n, p, c, ex, im) {
     this.posX = posx;
     this.posY = posy;
+    this.n = n;
+    this.p = p;
+    this.c = c;
+    this.ex = ex;
+    this.imag = imagenes[im];
   }
   dr(){
-//tamaño circulo de cada nodo
-    circle(this.posX, this.posY, windowHeight/7);
+    	image(this.imag, this.posX-((windowHeight/7)/2), this.posY-((windowHeight/7)/2), windowHeight/5, windowWidth/11)
   }
+  ln(){
+        stroke(this.c)
+        line(this.p*windowWidth/8-windowWidth/20, this.n*windowHeight/5-windowWidth/20, this.posX, this.posY)
+  }
+
 }
 
-function multiD(i){
-  a += 1
-  //posiciones de los nodos, divide pantalla en 13 verticalmente y en 6 horizontalmente
-  eval('c_nodo'+(1+(5*i))+' = new CNodo('+a+'*windowWidth/'+(13)+',windowHeight/'+6+')');
-  eval('c_nodo'+(2+(5*i))+' = new CNodo('+a+'*windowWidth/'+(13)+',2*windowHeight/'+6+')');
-  eval('c_nodo'+(3+(5*i))+' = new CNodo('+a+'*windowWidth/'+(13)+',3*windowHeight/'+6+')');
-  eval('c_nodo'+(4+(5*i))+' = new CNodo('+a+'*windowWidth/'+(13)+',4*windowHeight/'+6+')');
-  eval('c_nodo'+(5+(5*i))+' = new CNodo('+a+'*windowWidth/'+(13)+',5*windowHeight/'+6+')'); 
+function picture(){
+    im = random(imagenes.length)
+    console.log(int(im))
+    return int(im)
 }
 
-function execute(){
+function multiC(i,n){
+   a += 1
+   for(let h = 1; h <= 5; h++){
+	f = picture()
+	eval('c_nodo'+(h+(5*i))+' = new CNodo('+a+'*windowWidth/8-windowWidth/20'+',h*windowHeight/'+5+'-windowWidth/20,'+n+','+(a-1)+','+c+',ex,'+f+')');
+	imagenes.splice(f, 1)
+   }
+}
+
+function execute(n){
     eval('nodo'+i+'.multi('+i+')');
-    multiD(i);
+    multiC(i,n);
 }
 
 
 function keyPressed(){
   if (keyCode == 49){
-    console.log('1');
-    eval('nodo'+(i+1)+'= nodo'+i+'.n'+1);
-    i += 1;
-    execute();
+	eval('c_nodo'+(1+(5*i))+'.c = "yellow"');
+    if (exe == true){
+	eval('nodo'+(i+1)+'= nodo'+i+'.n'+1);
+    	i += 1;
+	n = 1;
+    	execute(n);
+    }
   }
+
   if (keyCode == 50){
-    console.log('2');
-    eval('nodo'+(i+1)+'= nodo'+i+'.n'+2);
-    i += 1;
-    execute();
+	eval('c_nodo'+(2+(5*i))+'.c = "yellow"');
+    if (exe == true){
+    	eval('nodo'+(i+1)+'= nodo'+i+'.n'+2);
+    	i += 1;
+	n = 2;
+    	execute(n);
+    }
   }
+
   if (keyCode == 51){
-    console.log('3');
-    eval('nodo'+(i+1)+'= nodo'+i+'.n'+3)
-    i += 1;
-    execute();
+	eval('c_nodo'+(3+(5*i))+'.c = "yellow"');
+    if (exe == true){
+    	eval('nodo'+(i+1)+'= nodo'+i+'.n'+3)
+    	i += 1;
+    	n = 3;
+    	execute(n);
+    }
   }
+
   if (keyCode == 52){
-    console.log('4');
-    eval('nodo'+(i+1)+'= nodo'+i+'.n'+4)
-    i += 1;
-    execute()
+	eval('c_nodo'+(4+(5*i))+'.c = "yellow"');
+    if (exe == true){
+    	eval('nodo'+(i+1)+'= nodo'+i+'.n'+4)
+    	i += 1;
+    	n = 4;
+    	execute(n);
+    }
   }
+
   if (keyCode == 53){
-    console.log('5');
-    eval('nodo'+(i+1)+'= nodo'+i+'.n'+5);
-    i += 1;
-    execute();
+	eval('c_nodo'+(5+(5*i))+'.c = "yellow"');
+    if (exe == true){
+    	eval('nodo'+(i+1)+'= nodo'+i+'.n'+5);
+    	i += 1;
+    	n = 5;
+    	execute(n);
+    }
   }
+
   if (keyCode == 88){
     exe = false;
+    console.log('X');
+  }
+
+  if (keyCode == 65){
     console.log('E');
+    if (exe == false){
+    }
+  }
+
+  if (keyCode == 69){
+    console.log('A');
+    if (exe == false){
+    }
   }
 }
-
